82a90f0b4c7b6e636bbf4bd062c1d10c9bb25f0c
afcrossland <andrew@example.com>
2026-06-13 18:30:26 +0100

    updated language, redefined manufesto. Added marginal pricing

--- assets/nav.js
+++ assets/nav.js
@@ -15,9 +15,9 @@ const NAV_HTML = `
             <li><a href="/last-28-days/index.html">Last Month</a></li>
             <li><a href="/last-12-months/index.html">Last 12 Months</a></li>
             <li><a href="/historicaldata/index.html">Generation History</a></li>
-            <li><a href="/2030grid/index.html">2030 Blueprint</a></li>
           </ul>
         </li>
+        <li><a href="/2030grid/index.html">2030 Blueprint</a></li>
         <li>
           <a href="/about/index.html">About ▾</a>
           <ul>
